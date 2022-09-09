84b8024e64bdc04055fdbbc1ad62683a2153fa28
selimtan <stan@bpmgenesis.com>
2022-09-09 17:00:40 +0300

    .

---
+++ src/pages/login.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Login() {
+   // window.location.replace('http://app.realmocean.com');
+    return (
+      <Layout title="Hello" description="Hello React Page">
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50vh',
+            fontSize: '20px',
+          }}>
+          <p>
+          Hire an expert
+          </p>
+        </div>
+      </Layout>
+    );
+  }
